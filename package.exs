35440d27b30cddabfe85be11cade6e1676f6a39e
Michael Truog <mjtruog@gmail.com>
2014-10-02 10:49:01 -0700

    Add hex.pm package.

---
+++ package.exs
@@ -0,0 +1,25 @@
+defmodule Pqueue.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :pqueue,
+     version: "1.3.3",
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+  end
+
+  defp description do
+    "Erlang Priority Queue Implementation"
+  end
+
+  defp package do
+    [files: ~w(src doc test Makefile rebar.config README.markdown),
+     contributors: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"GitHub" => "https://github.com/okeuday/pqueue"}]
+   end
+end
