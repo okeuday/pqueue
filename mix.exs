8bd1be1219f6d1739875ea38d98d43de2845c55d
Michael Truog <mjtruog@gmail.com>
2016-09-09 14:42:32 -0700

    Version 1.5.2.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Pqueue.Mixfile do
 
   def project do
     [app: :pqueue,
-     version: "1.5.1",
+     version: "1.5.2",
      description: description,
      package: package,
      deps: deps]
