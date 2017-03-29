a02ad6f44aab46d86028f0cb68dbaa7999c3ce48
Michael Truog <mjtruog@gmail.com>
2017-03-28 18:47:36 -0700

    Version 1.7.0.

--- mix.exs
+++ mix.exs
@@ -1,9 +1,12 @@
+#-*-Mode:elixir;coding:utf-8;tab-width:2;c-basic-offset:2;indent-tabs-mode:()-*-
+# ex: set ft=elixir fenc=utf-8 sts=2 ts=2 sw=2 et nomod:
+
 defmodule Pqueue.Mixfile do
   use Mix.Project
 
   def project do
     [app: :pqueue,
-     version: "1.6.0",
+     version: "1.7.0",
      description: description(),
      package: package(),
      deps: deps()]
